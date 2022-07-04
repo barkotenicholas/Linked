f6139268bfcb3685df4b663b174d6829fe48a894
Nicholas Barkote <barkotenicholas@gmail.com>
2022-07-04 22:59:33 +0300

    Added removeFront

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -9,6 +9,42 @@ public class LinkedList {
         node.setNext(head);
         head = node;
 
+        size++;
+    }
+
+    public int removeFront(){
+
+        if(isEmpty()){
+            return -1;
+        }
+        Node node = head;
+        head = head.getNext();
+        size --;
+        node.setNext(null);
+        return node.getData();
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Boolean isEmpty(){
+        return head == null;
+    }
+
+    public void printList(){
+        Node current = head;
+        System.out.print("HEAD -> ");
+        while (current != null){
+
+            System.out.println(current);
+
+            System.out.print("-> ");
+
+            current = current.getNext();
+        }
+        System.out.println("Null");
     }
 
 }
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.insertFront(1);
+        linkedList.insertFront(3);
+        linkedList.insertFront(4);
+        linkedList.insertFront(5);
+        linkedList.insertFront(9);
+
+        linkedList.printList();
+
+    }
+}
